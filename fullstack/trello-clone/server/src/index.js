874e53e83bed069c069d223f27a70a80d511a0ac
Huy Truong <huy.truong@enouvo.com>
2024-02-24 17:42:38 +0700

    trello_server: setup router

--- fullstack/trello-clone/server/src/index.js
+++ fullstack/trello-clone/server/src/index.js
@@ -3,13 +3,12 @@ import express from "express";
 import exitHook from "async-exit-hook";
 import { env } from "./config/environment.js";
 import { CLOSE_DB, CONNECT_DB } from "./config/mongodb.js";
+import { APIs_V1 } from "./routes/v1/index.js";
 
 const START_SERVER = () => {
   const app = express();
 
-  app.get("/", (req, res) => {
-    res.send("<h1>Hello World!</h1>");
-  });
+  app.use("/v1", APIs_V1);
 
   app.listen(env.PORT, env.APP_HOST, () => {
     console.log(`Server is running at http://${env.APP_HOST}:${env.PORT}/`);
---
+++ fullstack/trello-clone/server/src/routes/v1/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { boardRoute } from "./boardRoute.js";
+
+const Router = express.Router();
+
+Router.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Test API",
+  });
+});
+
+// Board APIs
+Router.use("/board", boardRoute);
+
+export const APIs_V1 = Router;
